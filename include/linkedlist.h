443391f9df36da4da5827274471656ec3176b776
Jett <fawwazna26@gmail.com>
2025-05-20 13:38:10 +0700

    struktur data

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -1,42 +1,15 @@
-/* linked_list.h */
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef char* infotype;
-
-typedef struct Node {
+typedef struct Tugas {
     infotype data;
-    struct Node* next;
-} Node;
-
-typedef struct {
-    Node* head;
-    int size;
-} LinkedList;
-
-// Fungsi inisialisasi linked list
-void initList(LinkedList* list);
-
-void insertFront(LinkedList* list, infotype data);
-
-void insertBack(LinkedList* list, infotype data);
-
-void insertAt(LinkedList* list, infotype data, int position);
-
-infotype deleteFront(LinkedList* list);
-
-infotype deleteBack(LinkedList* list);
-
-int deleteAt(LinkedList* list, int position);
-
-int search(LinkedList* list, infotype data);
-
-void printList(LinkedList* list);
-
-void clearList(LinkedList* list);
+    struct Tugas* next;
+} Tugas;
 
 #endif
